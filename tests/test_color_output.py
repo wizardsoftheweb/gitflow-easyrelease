92b492bb3942a672541aa2c4ec3001ccbe4b1ec7
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-11 04:15:16 -0500

    Define base test case

---
+++ tests/test_color_output.py
@@ -0,0 +1,45 @@
+# pylint: disable=missing-docstring
+
+from __future__ import print_function
+
+from unittest import TestCase
+
+from mock import call, MagicMock, patch
+
+from gitflow_easyrelease import ColorOutput
+
+
+class ColorOutputTestCase(TestCase):
+
+    def setUp(self):
+        self.construct_color_output()
+        self.addCleanup(self.wipe_color_output)
+
+    def wipe_color_output(self):
+        del self.color_output
+
+    def construct_color_output(self):
+        color_factory_patcher = patch.object(ColorOutput, 'color_factory')
+        self.mock_color_factory = color_factory_patcher.start()
+        self.color_output = ColorOutput()
+        color_factory_patcher.stop()
+
+
+class ConstructorUnitTests(ColorOutputTestCase):
+    """"""
+
+
+class ColorFactoryUnitTests(ColorOutputTestCase):
+    """"""
+
+
+class CallUnitTests(ColorOutputTestCase):
+    """"""
+
+
+class CanUseAnsiUnitTests(ColorOutputTestCase):
+    """"""
+
+
+class NoColorUnitTests(ColorOutputTestCase):
+    """"""
