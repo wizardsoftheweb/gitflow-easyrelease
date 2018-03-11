b1919bd8a9f8a4469587302c7c94a0453fd3d270
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-11 04:28:46 -0500

    Test color_factory

--- tests/test_color_output.py
+++ tests/test_color_output.py
@@ -32,7 +32,65 @@ class ConstructorUnitTests(ColorOutputTestCase):
 
 
 class ColorFactoryUnitTests(ColorOutputTestCase):
-    """"""
+    COLOR = MagicMock()
+    NO_COLOR = MagicMock()
+
+    def setUp(self):
+        ColorOutputTestCase.setUp(self)
+        color_patcher = patch(
+            'gitflow_easyrelease.color_output.color',
+            self.COLOR
+        )
+        self.mock_color = color_patcher.start()
+        self.addCleanup(color_patcher.stop)
+        no_color_patcher = patch(
+            'gitflow_easyrelease.color_output.ColorOutput.no_color',
+            self.NO_COLOR
+        )
+        self.mock_no_color = no_color_patcher.start()
+        self.addCleanup(no_color_patcher.stop)
+
+    @patch(
+        'gitflow_easyrelease.color_output.ColorOutput.can_use_ansi',
+        return_value=True
+    )
+    def test_with_ansi_color(self, mock_ansi):
+        mock_ansi.assert_not_called()
+        result = self.color_output.color_factory()
+        self.assertEqual(result, self.COLOR)
+        mock_ansi.assert_called_once_with()
+
+    @patch(
+        'gitflow_easyrelease.color_output.ColorOutput.can_use_ansi',
+        return_value=True
+    )
+    def test_blocked_color(self, mock_ansi):
+        self.color_output.block_color = True
+        mock_ansi.assert_not_called()
+        result = self.color_output.color_factory()
+        self.assertEqual(result, self.NO_COLOR)
+        mock_ansi.assert_not_called()
+
+    @patch(
+        'gitflow_easyrelease.color_output.ColorOutput.can_use_ansi',
+        return_value=False
+    )
+    def test_force_color(self, mock_ansi):
+        self.color_output.force_color = True
+        mock_ansi.assert_not_called()
+        result = self.color_output.color_factory()
+        self.assertEqual(result, self.COLOR)
+        mock_ansi.assert_called_once_with()
+
+    @patch(
+        'gitflow_easyrelease.color_output.ColorOutput.can_use_ansi',
+        return_value=False
+    )
+    def test_fall_through(self, mock_ansi):
+        mock_ansi.assert_not_called()
+        result = self.color_output.color_factory()
+        self.assertEqual(result, self.NO_COLOR)
+        mock_ansi.assert_called_once_with()
 
 
 class CallUnitTests(ColorOutputTestCase):
