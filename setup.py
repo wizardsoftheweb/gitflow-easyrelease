7db63bdddb65a1c015817c0d87e50cfff43d3b6e
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-11 09:55:18 -0500

    Move entry_points to setup.cfg

--- setup.py
+++ setup.py
@@ -5,9 +5,4 @@ from setuptools import setup
 setup(
     name='gitflow-easyrelease',
     include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'git-easyrelease = gitflow_easyrelease.cli_file:cli'
-        ]
-    }
 )
