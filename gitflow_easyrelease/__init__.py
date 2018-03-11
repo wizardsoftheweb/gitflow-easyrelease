391d1a1dfd0fc33ca6079995b55a5f5fd55c5246
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-11 00:27:22 -0600

    Import and use cli

--- gitflow_easyrelease/__init__.py
+++ gitflow_easyrelease/__init__.py
@@ -5,3 +5,7 @@ from .repo_info import RepoInfo
 from .color_output import ColorOutput
 from .subcommand import Subcommand
 from .application import Application
+from .cli import cli
+
+if '__main__' == __name__:
+    cli()
