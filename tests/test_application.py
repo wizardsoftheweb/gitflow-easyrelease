daf5e3f49b111a1a26e2fcb4e2a9538134118598
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-11 07:20:58 -0500

    Test bootstrap

--- tests/test_application.py
+++ tests/test_application.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 
+from argparse import Namespace
 from collections import OrderedDict
 from unittest import TestCase
 
@@ -86,7 +87,78 @@ class PopulateRootParserUnitTests(ApplicationTestCase):
 
 
 class BootstrapUnitTests(ApplicationTestCase):
-    """"""
+
+    def setUp(self):
+        ApplicationTestCase.setUp(self)
+        create_root_parser_patcher = patch.object(
+            Application,
+            'create_root_parser',
+            return_value=self.parser
+        )
+        self.mock_create_root_parser = create_root_parser_patcher.start()
+        self.addCleanup(create_root_parser_patcher.stop)
+        parse_args_patcher = patch(
+            'gitflow_easyrelease.application.Application.parse_args',
+            return_value=[MagicMock(spec=[]), MagicMock(spec=[])]
+        )
+        self.mock_parse_args = parse_args_patcher.start()
+        self.addCleanup(parse_args_patcher.stop)
+        populate_root_parser_patcher = patch.object(
+            Application,
+            'populate_root_parser'
+        )
+        self.mock_populate_root_parser = populate_root_parser_patcher.start()
+        self.addCleanup(populate_root_parser_patcher.stop)
+        print_all_help_patcher = patch(
+            'gitflow_easyrelease.application.Application.print_all_help'
+        )
+        self.mock_print_all_help = print_all_help_patcher.start()
+        self.addCleanup(print_all_help_patcher.stop)
+
+    def test_print_all_help(self):
+        parsed = Namespace()
+        parsed.all_help = True
+        options = []
+        self.mock_parse_args.return_value = [parsed, options]
+        self.mock_create_root_parser.assert_not_called()
+        self.mock_parse_args.assert_not_called()
+        self.mock_populate_root_parser.assert_not_called()
+        self.mock_print_all_help.assert_not_called()
+        self.mock_sub_execute.assert_not_called()
+        self.application.bootstrap(['--all-help'])
+        self.mock_create_root_parser.assert_called_once_with()
+        self.mock_parse_args.assert_called_once_with(
+            self.parser,
+            ['--all-help']
+        )
+        self.mock_populate_root_parser.assert_called_once_with(self.parser)
+        self.mock_print_all_help.assert_called_once_with(self.parser)
+        self.mock_sub_execute.assert_not_called()
+
+    def test_execute(self):
+        parsed = Namespace()
+        parsed.all_help = False
+        parsed.subcommand = self.SUBCOMMAND_KEY
+        options = ['--show-commands']
+        final = Namespace()
+        final.all_help = False
+        final.subcommand = self.SUBCOMMAND_KEY
+        final.options = options
+        self.mock_parse_args.return_value = [parsed, options]
+        self.mock_create_root_parser.assert_not_called()
+        self.mock_parse_args.assert_not_called()
+        self.mock_populate_root_parser.assert_not_called()
+        self.mock_print_all_help.assert_not_called()
+        self.mock_sub_execute.assert_not_called()
+        self.application.bootstrap([self.SUBCOMMAND_KEY, '--show-commands'])
+        self.mock_create_root_parser.assert_called_once_with()
+        self.mock_parse_args.assert_has_calls([
+            call(self.parser, [self.SUBCOMMAND_KEY, '--show-commands']),
+            call(self.parser, [self.SUBCOMMAND_KEY, '--show-commands'])
+        ])
+        self.mock_populate_root_parser.assert_called_once_with(self.parser)
+        self.mock_print_all_help.assert_not_called()
+        self.mock_sub_execute.assert_called_once_with(final)
 
 
 class CreateRootParserUnitTests(ApplicationTestCase):
