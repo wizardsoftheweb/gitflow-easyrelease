b9525dac8200c03dc4c47a76f0d545e9ee7e87c9
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-11 07:59:40 -0500

    Polish setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from os.path import join
+"""This file sets up the package"""
+
 from setuptools import setup, find_packages
 
 setup(
@@ -8,7 +9,7 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'git-easyrelease = gitflow_easyrelease.cli:cli'
+            'git-easyrelease = gitflow_easyrelease.cli_file:cli'
         ]
     }
 )
