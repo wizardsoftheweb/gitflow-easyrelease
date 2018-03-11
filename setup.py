000d3a9626ea1babc5e29983f6c9abd206ea0d54
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-11 08:06:47 -0500

    Bump version: 0.0.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gitflow-easyrelease',
-    version='0.0.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     entry_points={
